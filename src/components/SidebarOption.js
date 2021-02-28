dc9d0b2d3eabbe8c38f3fd0ed757ab7e91c083a4
taurusilver7 <prasad20697@gmail.com>
2021-02-28 19:23:36 +0530

    sidebar comp ind comp

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import { enterRoom } from "../features/appSlice";
+import { db } from "../firebase";
+
+const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
+  const dispatch = useDispatch();
+
+  const addChannel = () => {
+    const channelName = prompt("Please enter a channel name..");
+
+    if (channelName) {
+      db.collection("rooms").add({
+        name: channelName,
+      });
+    }
+  };
+
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
+
+  return (
+    <SidebarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
+      {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <SidebarOptionChannel>
+          <span>#</span> {title}
+        </SidebarOptionChannel>
+      )}
+    </SidebarOptionContainer>
+  );
+};
+
+export default SidebarOption;
+
+const SidebarOptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+  padding-left: 2px;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.9;
+    background-color: #340e36;
+  }
+
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
+
+  @media (max-width: 425px) {
+    > h3 {
+      font-size: x-small;
+    }
+    > .MuiSvgIcon-root {
+      font-size: x-small;
+    }
+  }
+`;
+
+const SidebarOptionChannel = styled.h3`
+  padding: 10px 0;
+  font-weight: 300;
+`;
